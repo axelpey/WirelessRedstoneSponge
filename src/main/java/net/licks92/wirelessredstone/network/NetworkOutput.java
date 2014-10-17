924a4ec3574ac3f39f773d0b343e7d996ed4e4bf
Saladoc <saladoc@heridan.de>
2014-10-17 16:23:04 +0200

    Use abstract outputbehaviours for output elements

--- src/main/java/net/licks92/wirelessredstone/network/NetworkOutput.java
+++ src/main/java/net/licks92/wirelessredstone/network/NetworkOutput.java
@@ -1,27 +1,39 @@
 package net.licks92.wirelessredstone.network;
 
+import net.licks92.wirelessredstone.network.output.OutputBehaviour;
 import net.licks92.wirelessredstone.util.Position;
 
 /**
  * A generic output element
  */
 public abstract class NetworkOutput extends NetworkElement {
+    
+    private OutputBehaviour output;
+    private boolean isActive;
 
-    public NetworkOutput(Position pos) {
+    public NetworkOutput(Position pos, OutputBehaviour output) {
         super(pos);
+        this.output = output;
     }
 
     /**
      * Checks if the output element is active
      */
     public boolean isActive() {
-        return false; // TODO Implementation
+        return this.isActive;
     }
     
     /**
      * Updates the Activation status of this element
      */
     public void setIsActive(boolean status) {
-        // TODO Implementation
+        if (status != this.isActive) {
+            this.isActive = status;
+            if (this.isActive) {
+                this.output.activate();
+            } else {
+                this.output.deactivate();
+            }
+        }
     }
 }
---
+++ src/main/java/net/licks92/wirelessredstone/network/output/OutputBehaviour.java
@@ -0,0 +1,12 @@
+package net.licks92.wirelessredstone.network.output;
+
+/**
+ * A Behaviour applicable to a NetworkOutput
+ */
+public interface OutputBehaviour {
+    
+    public void activate();
+    
+    public void deactivate();
+
+}
